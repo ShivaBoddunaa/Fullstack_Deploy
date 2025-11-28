4f9d7773016489a134e7f2011b253676fd42ca9d
shiva bodduna <shivabodduna123@gmail.com>
2025-11-28 17:10:07 +0530

    Url commit

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DB_URL = os.getenv('POSTGRESS_URL')
+DB_URL = os.getenv("postgresql://postgres.plfcbrrmqulhjavgkyhf:1234567@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres")
 
 
 def get_conn():
